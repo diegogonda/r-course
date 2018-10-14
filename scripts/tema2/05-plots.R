96004a38e200a4f589cae5aa77ada7fc82979575
Diego <diegoperezgonda@gmail.com>
2018-10-15 01:08:55 +0200

    Update 05-plots.R

--- scripts/tema2/05-plots.R
+++ scripts/tema2/05-plots.R
@@ -1,11 +1,19 @@
+# #d representaciones
 auto <- read.csv("../data/tema2/auto-mpg.csv")
+# #d sobreescribimos la columna cilindros para que sea una categoria
 auto$cylinders <- factor(auto$cylinders, 
                          levels = c(3,4,5,6,8),
                          labels = c("3cil", "4cil", "5cil", "6cil", "8cil"))
+# #d hace que las variables de auto sean variables de R
+# #d con lo que podemos instanciarlos directamente, ahorrándonos los $
 attach(auto)
 head(cylinders)
 
 #Histograma de frecuencias
+# #d representación de la aceleración del dataframe de la aceleración
+# #d nos permite ver la forma de los datos (medidas de centralización, dispersión)
+# #d parametro break: número de divisiones
+# #d Frecuencias: Cantidad de veces que se repite
 hist(acceleration,
      col = rainbow(12),
      xlab = "Aceleración",
@@ -13,18 +21,23 @@ hist(acceleration,
      main = "Histograma de las aceleraciones",
      breaks = 12)
 
+# #d prob
 hist(mpg, breaks = 16, prob = T)
 lines(density(mpg))
 
 
 #Boxplots
+# #d diagrama de caja y bigotes
 boxplot(mpg, xlab = "Millas por Galeón")
 boxplot(mpg ~ model_year, xlab = "Millas por Galeón (por año)")
 boxplot(mpg ~ cylinders, xlab = "Consumo por Número de cilindros")
 boxplot(auto)
 
 #Scatterplot
-plot(mpg ~ horsepower, type = "n")
+# #d pintar dos variables númericas: la primera en x y la segunda en y
+# #d nos puede indicar como las variables se relacionan entre ellas
+# #d por ejemplo la potencia con el consumo, el peso con el consumo, u otros..
+plot(mpg ~ horsepower, type = "n") # #d (y, x) sirve para ver tendencias
 linearmodel <- lm(mpg ~ horsepower)
 abline(linearmodel)
 #Agregar colores para cada cilindrada
@@ -39,6 +52,7 @@ with(subset(auto, cylinders == "4cil"),
 with(subset(auto, cylinders == "3cil"),
      points(horsepower, mpg))
 #Matriz de Scatterplots
+# #d indicamos las variables que creemos que tienen relación
 pairs(~mpg+displacement+horsepower+weight)
 
 
