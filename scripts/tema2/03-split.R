aed34998eb94b0fb1c23089ab0c5b3f05d27a523
Diego <diegoperezgonda@gmail.com>
2018-10-14 13:32:54 +0200

    Update 03-split.R

--- scripts/tema2/03-split.R
+++ scripts/tema2/03-split.R
@@ -1,11 +1,13 @@
 #split / unsplit
 data <- read.csv("../data/tema2/auto-mpg.csv", stringsAsFactors = F)
 
+# #d divisiones de un dataframe para obtener datos
+# #d en este caso hacemos una lista de 5 elementos (3 cilindros, 4, 5, 6 y 8)
 carslist <- split(data, data$cylinders)
-carslist[1]
-carslist[[1]]
+carslist[1] # #d el primero de la lista, los coches con 3 cilindros
+carslist[[1]] # #d dataframe de carslist[1]
 
-
+# #d structura de del list
 str(carslist[1])
 
 names(carslist[[1]])
