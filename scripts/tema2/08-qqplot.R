492263bf5834f83d4a7272574a9b96e9646d7143
Diego <diegoperezgonda@gmail.com>
2018-10-16 17:29:14 +0200

    Update 08-qqplot.R

--- scripts/tema2/08-qqplot.R
+++ scripts/tema2/08-qqplot.R
@@ -1,3 +1,24 @@
+# #d Vídeo 51: La técnica de validación cruzada: Cross Validation
+# #d se usa para evitar que las particiones de datos estean sesgadas
+# #d debemos realizar diferentes modelizaciónes para quedarnos con la mejor de ellas
+# #d se obtene la medida del error y se obtiene una media de este para obtener cuál es la calidad del sistema
+# #d se analizan los siguiente métodos
+
+# #d Validación cruzada de K iteraciones
+# #d lo tipo es k = 10 => ten fold cross validation
+
+# #d Random Cross Validation 
+# #d Escogemos llos datos de prueba de forma aleatoria
+
+# #d Leave one out Cross Validation
+# #d Todos los datos son de entrenamiento menos uno que es de prueba
+# #d es muy bueno pero tiene demasiado coste computacional
+
+
+# #d Video 52: Gráficos quantil
+# #d Los cuantiles o percentiles, son puntos específicos de la muestra que dejan 
+# #d parte de datos por debajo de ellos (por ejemplo la distribución normal de Gauss, la campana de Gauss)
+# #d 
 s <- seq(0.01, 0.99, 0.01)
 s
 qn <- qnorm(s)
